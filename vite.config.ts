75bb1ed9f3ee7dd47b29d188a54b9ec7be9b06f4
young24n <won8560@gmail.com>
2026-04-10 14:31:57 +0900

    feat: 예제 구현, 기능 확인을 위한 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  resolve: { // @로 경로 별명 설정
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
 })
